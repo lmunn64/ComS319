b0dd4cbcee64e559d464334478ec6e7331c892b4
unknown <lmunn64@iastate.edu>
2024-03-09 18:22:37 -0600

    working collection display, and fixed some text sizes

--- Midterm Project/index.js
+++ Midterm Project/index.js
@@ -1,87 +1,62 @@
-async function searchSong() {
-	const trackName = document.getElementById('searchInput').value;
-	const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-	const apiUrl = `https://api.deezer.com/search?q=track:"${trackName}"`;
-	const response = await fetch(proxyUrl + apiUrl);
-	const data = await response.json();
-	console.log(data);
-	displaySearchResults(data.data);
-}
 
-function displaySearchResults(data) {
-	const searchResultsElement = document.getElementById('searchResults');
-	searchResultsElement.innerHTML = '';
-
-	data.forEach((track) => {
-		const trackElement = document.createElement('div');
-
-		trackElement.id = track.id;
-		trackElement.innerHTML = `
-		<div class="row">
-		<div class="col-md-2">
-			<h3>${track.title}</h3>
-			<img 
-			src="${track.album.cover}"
-			style="height: 7rem; width: 7rem; object-fit: cover; box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); border: 0.35rem solid white;"
-			/>
-			<p>Album: ${track.album.title}</p>
-		</div>
-		<div class="col-md-4 my-auto d-flex align-items-center justify-content-center">
-        <audio controls>
-            <source src="${track.preview}" type="audio/mp3">
-            Your browser does not support the audio element.
-        </audio>
-    </div>
-		<div class="col-md-4 d-flex align-items-center">
-        <img 
-            src="${track.artist.picture}" 
-            style="height: 6rem; width: 6rem; object-fit: cover; box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); border-radius: 50%; border: 0.35rem solid white;" 
-        />
-        <p class="ml-3">Artist: ${track.artist.name}</p>
-    </div>
-    <div class="col-md-2 my-auto d-flex align-items-center justify-content-center">
-        <button class="btn btn-primary"
-		style="height: 3rem; width: 3rem; object-fit: cover; box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); border-radius: 50%; border: 0.35rem solid white;"
-		onclick="toggleButton(this); addToCollection(${track.id})">+</button>
-    </div>
-	</div>
-	
-	`;
-		searchResultsElement.appendChild(trackElement);
-	});
-}
-
-const idCollection = [];
-const collection = [];
-
-async function addToCollection(trackId) {
-	if (!idCollection.includes(trackId)) {
-		idCollection.push(trackId);
-		try {
-			// Fetch detailed information of the track using its ID
-			const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-			const trackUrl = `https://api.deezer.com/track/${trackId}`;
-			const response = await fetch(proxyUrl + trackUrl);
-			if (!response.ok) {
-				throw new Error('Failed to fetch track data');
-			}
-			const trackData = await response.json();
-
-			// Add the trackObj instance to the collection
-			collection.push(trackData);
-			window.sessionStorage.setItem("Collection", JSON.stringify(collection));
-			console.log('Song added: ', trackData);
-		} catch (error) {
-			console.error('Error adding song to collection: ', error);
-		}
-		console.log('Current collection: ', JSON.parse(sessionStorage.getItem("Collection")));
-	} else {
-		console.log('song in collection already');
+function displayCollection(){ 
+    var catalogue = JSON.parse(sessionStorage.getItem("Collection"))
+    if(!catalogue){
+        var text = document.createElement("h3");
+        text.setAttribute("style", "text-align: center;")
+        text.innerText = "No Music! Go to the Search tab to add music to your collection."
+        document.getElementById("main").appendChild(text);
+	}
+    else{
+        for(var i = 0; i < catalogue.length; i++){
+            var title = catalogue[i].title;
+            var artist = catalogue[i].artist.name;
+            var cover = catalogue[i].album.cover_medium;
+            var preview = catalogue[i].preview;
+            createCards(cover, artist, title, preview);
+        }
 	}
 }
 
-function toggleButton(button) {
-	button.classList.remove('btn-primary');
-	button.classList.add('btn-success');
-	button.innerHTML = '✔';
-}
+function createCards(cover, artist, title, preview){
+    var divCard = document.createElement("div");
+    divCard.setAttribute("class", "col");
+    
+    var img = document.createElement('img');
+    img.setAttribute("src", cover);
+    img.setAttribute("class", "card-img-top");
+
+    divCard.append(img);
+
+    var cardBody = document.createElement("div");
+    cardBody.setAttribute("class", "card-body");
+
+    var cardText = document.createElement("p");
+    cardText.setAttribute("class","card-text");
+    cardText.innerText = artist;
+
+    var cardTitle = document.createElement("h5");
+    cardTitle.setAttribute("class","card-title");
+    cardTitle.innerText = title;
+
+    var audio = document.createElement("audio");
+    audio.setAttribute("controls", "controls");
+    audio.setAttribute("class", "w-100");
+
+    var source = document.createElement("source");
+    source.setAttribute("src", preview);
+    source.setAttribute("type", "audio/mp3");
+
+    audio.append(source);
+  
+    cardBody.append(cardTitle);
+    cardBody.append(cardText);
+    cardBody.append(audio);
+    
+
+    divCard.append(cardBody);
+
+    document.getElementById("catalogueCards").appendChild(divCard); 
+    console.log(divCard)
+  }
+
